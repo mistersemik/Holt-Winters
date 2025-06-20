d97a1b8e0d420b31b9ba73869e00309ab1805783
mister.semik <Seminanikin@mail.ru>
2025-06-20 13:59:54 +0400

    Расчёт ошибок

--- core/calculations.py
+++ core/calculations.py
@@ -4,7 +4,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 def calculate_metrics(forecast, actual):
     """Расчет метрик ошибок"""
     forecast = forecast[:len(actual)]
-    mae = mean_absolute_error(actual, forecast)
-    rmse = np.sqrt(mean_squared_error(actual, forecast))
-    mape = np.mean(np.abs((actual - forecast) / actual)) * 100
+    mae = mean_absolute_error(actual, forecast) # Средняя абсолютная ошибка
+    rmse = np.sqrt(mean_squared_error(actual, forecast)) # Среднеквадратичная ошибка
+    mape = np.mean(np.abs((actual - forecast) / actual)) * 100 # Средняя абсолютная процентная ошибка
     return mae, rmse, mape
