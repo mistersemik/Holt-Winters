f148a2e48ae708bf17ace38eab1757af92d4c630
mister.semik <Seminanikin@mail.ru>
2025-07-04 13:02:06 +0400

    Убрал избыточное построение моделей HW. Сейчас реализова прогноз, построенный на созданной ранее модели hw_model

--- main.py
+++ main.py
@@ -232,13 +232,8 @@ def main():
 
     # Прогнозирование с кластеризацией
     try:
-        # Построение моделей
-        hw_add_model = build_model(ts, trend='add', seasonal_type='add')
-        hw_mul_model = build_model(ts, trend='mul', seasonal_type='mul')
-
-        # Получаем кластерные прогнозы
-        cluster_forecast_add, weights_add = clustered_hw(ts, hw_model=hw_add_model)
-        cluster_forecast_mul, weights_mul = clustered_hw(ts, hw_model=hw_mul_model)
+        cluster_forecast_add, weights_add = clustered_hw(ts, hw_model=model_add)
+        cluster_forecast_mul, weights_mul = clustered_hw(ts, hw_model=model_mul)
 
         # Выводим результаты
         print("\nВеса кластеров (аддитивная модель):")
