c939b8106c894089c7acd0c04f99b1ea017eac55
mister.semik <Seminanikin@mail.ru>
2025-07-05 17:30:42 +0400

    Holt-Winters Forecast v 0.10.2

--- __version__.py
+++ __version__.py
@@ -1,4 +1,4 @@
 # core/__version__.py
-__version__ = "0.10.1"
-__version_info__ = (0, 10, 1)
+__version__ = "0.10.2"
+__version_info__ = (0, 10, 2)
 version_str = f"Holt-Winters Forecast v {__version__}"
